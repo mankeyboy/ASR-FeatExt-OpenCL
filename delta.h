dd90f3d6c4924ec253777a23cb8d22cd159e804e
Mayank Roy <mayank.roy812@gmail.com>
2016-10-27 16:52:34 +0530

    Forgot to add header files, adding now

---
+++ delta.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Delta
+{
+private:
+	int m_dim,
+		m_window_limit,
+		m_delta_size;
+	float * m_output;
+public:
+	Delta() : m_dim(0), m_window_limit(0), m_output(nullptr) {}
+	void init(int dim, int window_limit, int delta_size);
+	void cleanup();
+
+	void apply(const float * data, int window_count);
+	float * get_output_buffer() { return m_output; }
+};
